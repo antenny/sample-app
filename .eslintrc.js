bba592c1eab89eef9538a00dba7c52c4d455ea22
Jake Breindel <j.breindel2@outlook.com>
2020-12-31 16:04:44 -0500

    Added eslint.

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  rules: {
+    indent: 0,
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    quotes: [
+      'error',
+      'single'
+    ],
+    semi: [
+      'error',
+      'always'
+    ],
+    'no-console': 0
+  }
+};
