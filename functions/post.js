7f79b005e06a3005137de466f5c8e53609345fd0
Jake Breindel <j.breindel2@outlook.com>
2020-12-30 22:44:16 -0500

    Added code to put trades into database.

---
+++ functions/post.js
@@ -0,0 +1,139 @@
+const crypto = require('crypto');
+const AWS = require('aws-sdk');
+const client = new AWS.DynamoDB.DocumentClient({
+  region: process.env.AWS_REGION
+});
+const cors = {
+  'Access-Control-Allow-Headers': '*',
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'POST'
+};
+
+exports.handler = async evt => {
+  console.log(JSON.stringify(evt));
+  if (evt.httpMethod !== 'POST') {
+    return {
+      statusCode: 405,
+      headers: cors
+    };
+  }
+  if (!checkSig(evt)) {
+    return {
+      statusCode: 400,
+      headers: cors
+    };
+  }
+  const body = parseKey(evt, 'body');
+  if (!body) {
+    return {
+      statusCode: 400,
+      headers: cors
+    };
+  }
+  if (body.event !== 'received') {
+    // pass on other events
+    return {
+      statusCode: 200,
+      headers: cors
+    };
+  }
+  const msg = parseKey(body, 'message');
+  if (!msg) {
+    return {
+      statusCode: 400,
+      headers: cors
+    };
+  }
+  await processMsg(msg);
+  return {
+    statusCode: 200,
+    headers: cors
+  };
+};
+
+const checkSig = evt => {
+  const sig = evt.headers && evt.headers['X-Antenny-Sig'];
+  if (!sig) {
+    console.error('No Signature!');
+    return false;
+  }
+  const body = evt.body;
+  if (!body) {
+    console.error('No Body Included!');
+    return false;
+  }
+  const secret = process.env.ANTENNY_SECRET;
+  const hmac = crypto.createHmac('sha256', secret);
+  hmac.update(body);
+  if (sig !== hmac.digest('base64')) {
+    console.error('Bad Signature!');
+    return false;
+  }
+  return true;
+};
+
+const parseKey = (parse, key) => {
+  if (!parse[key]) {
+    console.error(`No key for: ${key}!`);
+    return false;
+  }
+  let parsed;
+  try {
+    parsed = JSON.parse(parse[key]);
+  } catch (err) {
+    console.error(err.message);
+    return null;
+  }
+  return parsed;
+};
+
+const processMsg = async msg => {
+  const evts = msg.events;
+  if (!Array.isArray(evts)) {
+    return;
+  }
+  for (let i = 0; i < evts.length; i++) {
+    const evt = evts[i];
+    const tradeId = evt.tid;
+    const price = evt.price;
+    const amount = evt.amount;
+    const makerSide = evt.makerSide;
+    if (!tradeId || !price || !amount || !makerSide) {
+      continue;
+    }
+    const key = getKey(evt);
+    if (!key) {
+      continue;
+    }
+    const { partitionKey, sortKey } = key;
+    const created = new Date().toISOString();
+    await client.put({
+      TableName: process.env.TABLE_NAME,
+      Item: {
+        partitionKey,
+        sortKey,
+        tradeId,
+        price,
+        amount,
+        makerSide,
+        created
+      }
+    }).promise();
+  }
+};
+
+const getKey = evt => {
+  const millis = evt.timestampms;
+  if (isNaN(millis)) {
+    return null;
+  }
+  const five = 1000 * 60 * 5;
+  const round = Math.floor(millis / five) * five;
+  const date = new Date(0);
+  date.setUTCMilliseconds(round);
+  const inter = date.toISOString();
+  return {
+    partitionKey: inter,
+    sortKey: millis
+  };
+};
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const core = require('@aws-cdk/core');
 const iam = require('@aws-cdk/aws-iam');
 const ssm = require('@aws-cdk/aws-ssm');
 const secretsmanager = require('@aws-cdk/aws-secretsmanager');
+const dynamodb = require('@aws-cdk/aws-dynamodb');
 const lambda = require('@aws-cdk/aws-lambda');
 const apigateway = require('@aws-cdk/aws-apigatewayv2');
 const integrations = require('@aws-cdk/aws-apigatewayv2-integrations');
@@ -12,51 +13,90 @@ const antenny = require('antenny-cdk');
 class ExampleStack extends core.Stack {
   constructor(scope, id, props) {
     super(scope, id, props);
-    const apiFnRole = new iam.Role(this, 'ApiFnRole', {
+    const table = new dynamodb.Table(this, 'Table', {
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      partitionKey: {
+        type: dynamodb.AttributeType.STRING,
+        name: 'partitionKey'
+      },
+      sortKey: {
+        type: dynamodb.AttributeType.NUMBER,
+        name: 'sortKey'
+      }
+    });
+    table.addLocalSecondaryIndex({
+      indexName: 'partitionKeyByPriceIdx',
+      projectionType: dynamodb.ProjectionType.ALL,
+      sortKey: {
+        type: dynamodb.AttributeType.NUMBER,
+        name: 'price'
+      }
+    });
+    const postFnRole = new iam.Role(this, 'PostFnRole', {
       assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
       managedPolicies: [
         iam.ManagedPolicy.fromAwsManagedPolicyName(
           'service-role/AWSLambdaBasicExecutionRole'
         )
-      ]
+      ],
+      inlinePolicies: {
+        dynamodb: new iam.PolicyDocument({
+          statements: [
+            new iam.PolicyStatement({
+              sid: 'dynamodb',
+              effect: iam.Effect.ALLOW,
+              actions: ['dynamodb:PutItem'],
+              resources: [table.tableArn]
+            })
+          ]
+        })
+      }
     });
-    const apiFn = new lambda.Function(this, 'ApiFn', {
-      role: apiFnRole,
+    const custSecret = secretsmanager.Secret.fromSecretNameV2(
+      this,
+      'CustSecret',
+      'atyCustSecret'
+    );
+    const postFn = new lambda.Function(this, 'PostFn', {
+      role: postFnRole,
       runtime: lambda.Runtime.NODEJS_12_X,
       handler: 'index.handler',
       memorySize: 256,
       timeout: core.Duration.seconds(30),
       code: lambda.Code.fromInline(fs.readFileSync(path.join(
         __dirname,
-        'function',
-        'index.js'
-      ), { encoding: 'utf8' }))
+        'functions',
+        'post.js'
+      ), { encoding: 'utf8' })),
+      environment: {
+        ANTENNY_SECRET: custSecret.secretValue.value
+      }
     });
-    const apiFnInteg = new integrations.LambdaProxyIntegration({
-      handler: apiFn
+    const postFnInteg = new integrations.LambdaProxyIntegration({
+      handler: postFn
     });
     const api = new apigateway.HttpApi(this, 'Api', {
       corsPreflight: {
         allowHeaders: ['Authorization', 'X-Antenny-Sig'],
-        allowMethods: [apigateway.HttpMethod.ANY],
+        allowMethods: ['*'],
         allowOrigins: ['*'],
         maxAge: core.Duration.days(10)
       }
     });
     api.addRoutes({
       path: '/endpoint',
-      methods: [apigateway.HttpMethod.ANY],
-      integration: apiFnInteg
+      methods: [apigateway.HttpMethod.POST],
+      integration: postFnInteg
     });
-    const secret = secretsmanager.Secret.fromSecretNameV2(
+    const apiSecret = secretsmanager.Secret.fromSecretNameV2(
       this,
-      'Secret',
-      'antennyApiKey'
+      'ApiSecret',
+      'atyApiKey'
     );
     const sub = new antenny.Subscription(this, 'Sub', {
-      apiKey: secret.secretValue.value,
+      apiKey: apiSecret.secretValue.value,
       subscription: {
-        name: 'example-subscription',
+        name: 'test-subscription',
         customerId: ssm.StringParameter.valueFromLookup(
           this,
           '/antenny/customerId'
@@ -64,7 +104,8 @@ class ExampleStack extends core.Stack {
         region: this.region,
         resource: {
           protocol: 'ws',
-          url: ''
+          url: 'wss://api.gemini.com/v1/marketdata/BTCUSD/'
+           + '?bids=false&offers=false&auctions=false&trades=true'
         },
         endpoint: {
           protocol: 'http',
